db5781f1f08f0c110460b2516fa2a024f26273f4
Ally Netzel <netzelally@gmail.com>
2018-05-18 22:39:35 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,8 @@
 INSERT INTO series VALUES (1, "Goats", 1, 1);
 INSERT INTO series VALUES (2, "Chicks", 2, 2);
 
-INSERT INTO books VALUES (2, "Chicks", 2, 2);
+INSERT INTO books VALUES (1, "sky", "2000", 1):
+
 INSERT INTO books VALUES (2, "Chicks", 2, 2);
 INSERT INTO books VALUES (2, "Chicks", 2, 2);
 INSERT INTO books VALUES (2, "Chicks", 2, 2);
