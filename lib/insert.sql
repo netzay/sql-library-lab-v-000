8bf693271656b2b8e410f6760220057fc6d4d94f
Ally Netzel <netzelally@gmail.com>
2018-05-18 22:44:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -18,6 +18,10 @@ INSERT INTO characters VALUES (7, "Salt", "bunny", "Bop", 6, 7);
 INSERT INTO characters VALUES (8, "Spoon", "bear", "Boop", 5, 10);
 
 
+INSERT INTO subgenre VALUES (1, "Julie", "Dog", "Hey", 1, 1);
+
+INSERT INTO characters VALUES (1, "Julie", "Dog", "Hey", 1, 1);
+
 
 
 
