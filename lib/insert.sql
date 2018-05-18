e24070dbfbb861e4f513ddba008d742ce590d634
Ally Netzel <netzelally@gmail.com>
2018-05-18 22:44:32 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,6 +9,15 @@ INSERT INTO books VALUES (5, "ocean", "2001", 5);
 INSERT INTO books VALUES (6, "rock", "2005", 6);
 
 INSERT INTO characters VALUES (1, "Julie", "Dog", "Hey", 1, 1);
+INSERT INTO characters VALUES (2, "Poppy", "whale", "Hi", 2, 4);
+INSERT INTO characters VALUES (3, "Curly", "cat", "Hello", 4, 6);
+INSERT INTO characters VALUES (4, "Jorge", "duck", "Bye", 3, 5);
+INSERT INTO characters VALUES (5, "Mike", "dove", "Cool", 7, 8);
+INSERT INTO characters VALUES (6, "Pepper", "chicken", "Grr", 9, 3);
+INSERT INTO characters VALUES (7, "Salt", "bunny", "Bop", 6, 7);
+INSERT INTO characters VALUES (8, "Spoon", "bear", "Boop", 5, 10);
+
+
 
 
 
