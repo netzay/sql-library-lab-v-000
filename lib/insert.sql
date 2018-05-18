b28c93838bd7b670a37d27c74b300724f87fd9e4
Ally Netzel <netzelally@gmail.com>
2018-05-18 22:40:39 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,14 +1,14 @@
 INSERT INTO series VALUES (1, "Goats", 1, 1);
 INSERT INTO series VALUES (2, "Chicks", 2, 2);
 
-INSERT INTO books VALUES (1, "sky", "2000", 1);
-INSERT INTO books VALUES (1, "sky", "2000", 1);
+INSERT INTO books VALUES (1, "sky", "2010", 1);
+INSERT INTO books VALUES (2, "grass", "2009", 2);
+INSERT INTO books VALUES (3, "mountain", "2004", 3);
+INSERT INTO books VALUES (4, "pond", "2000", 4);
+INSERT INTO books VALUES (5, "ocean", "2001", 5);
+INSERT INTO books VALUES (6, "rock", "2005", 6);
 
 
-INSERT INTO books VALUES (2, "Chicks", 2, 2);
-INSERT INTO books VALUES (2, "Chicks", 2, 2);
-INSERT INTO books VALUES (2, "Chicks", 2, 2);
-INSERT INTO books VALUES (2, "Chicks", 2, 2);
-INSERT INTO books VALUES (2, "Chicks", 2, 2);
+
 
 
