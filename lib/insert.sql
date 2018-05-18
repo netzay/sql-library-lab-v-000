7e169353369fb7da1afe93e566dbc5c628c6a7e0
Ally Netzel <netzelally@gmail.com>
2018-05-18 22:38:43 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,11 @@
 INSERT INTO series VALUES (1, "Goats", 1, 1);
 INSERT INTO series VALUES (2, "Chicks", 2, 2);
 
+INSERT INTO books VALUES (2, "Chicks", 2, 2);
+INSERT INTO books VALUES (2, "Chicks", 2, 2);
+INSERT INTO books VALUES (2, "Chicks", 2, 2);
+INSERT INTO books VALUES (2, "Chicks", 2, 2);
+INSERT INTO books VALUES (2, "Chicks", 2, 2);
+INSERT INTO books VALUES (2, "Chicks", 2, 2);
+
+
