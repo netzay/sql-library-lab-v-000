aa17b60991f141518fff71586e5b0d64c0b3f69b
Ally Netzel <netzelally@gmail.com>
2018-05-18 22:50:31 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -22,6 +22,9 @@ INSERT INTO subgenres VALUES (2, "Fantasy");
 
 INSERT INTO authors VALUES (1, "Beth");
 INSERT INTO authors VALUES (2, "Deb");
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
 
 
 
